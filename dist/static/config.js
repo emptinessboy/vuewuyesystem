fd013a43b22004427e99f9bd1aef2268f02eea43
EmptinessBoy <i@my.huxiaofan.com>
2020-11-28 11:56:50 +0800

    - 生产目录打包

---
+++ dist/static/config.js
@@ -0,0 +1,9 @@
+//存放后端api路径等全局控制文件，免得每次编译
+// eslint-disable-next-line no-unused-vars
+const hxf_conf = {
+  BaseUrl: "http://127.0.0.1:8080",
+  // BaseUrl: "http://localhost:8081/remoapi/api/",
+  hxf_fangjian: "500",
+  appkey: "",
+  token: ""
+};
